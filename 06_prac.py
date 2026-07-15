a169bbc1c58fcd885a646d509fdf76921d335b9a
cha-sy <gch05618@naver.com>
2026-07-15 17:51:55 +0900

    06_실습

---
+++ 06_prac.py
@@ -0,0 +1,17 @@
+# 실습 1
+a = 17
+b = 5
+print(a + b)  # 22
+print(a - b)  # 12
+print(a * b)  # 85
+print(a / b)  # 3.4
+print(a // b)  # 3
+print(a % b)  # 2
+print(a**b)  # 1419857
+
+# 실습 2
+x, y, z = 30, 40, 50
+print((x + y + z) / 3)  # 40.0
+side = 5
+print(5**2)  # 25
+print(5 * 6 * 7)  # 210
