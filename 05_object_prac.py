98f043528e76171151b3b65494d4e8370eb568b3
cha-sy <gch05618@naver.com>
2026-07-15 17:41:31 +0900

    실습

---
+++ 05_object_prac.py
@@ -0,0 +1,42 @@
+# 실습 1
+count = 5  # int
+temp = 36.8  # float
+name = "센서 A"  # str
+is_ok = True  # bool
+print(count, temp, name, is_ok)  # 5 36.8 센서A True
+
+# 실습 2
+print(type(count))  # <class 'int'>
+print(type(temp))  # <class 'float'>
+print(type(name))  # <class 'str'>
+print(type(is_ok))  # <class 'bool'>
+
+# 실습 3
+print(type(100))  # <class 'int'>
+print(type(100.0))  # <class 'float'>
+print(type("100"))  # <class 'str'>
+
+# 실습 4
+print(6 + 8)  # 14
+print("6" + "8")  # 68
+print("14" + "16")  # 1416
+
+# 실습 5
+print(3 > 2)  # True
+print(5 == 5)  # True
+print(type(3 > 2))  # <class 'bool'>
+
+# 실습 6
+count = 7
+print(count, type(count))  # 7 <class 'int'>
+count = 7.0
+print(count, type(count))  # 7.0 <class 'float'>
+count = "7"
+print(count, type(count))  # 7 <class 'str'>
+
+# 실습 7
+device_temp = 36.8  # float
+check_count = 77  # int
+device_name = "센서 A"  # str
+is_normal = False  # bool
+
